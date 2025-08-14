60e62316c28ca9103b11d70140ec30c16cee7000
hirosec <90599855+hirosec@users.noreply.github.com>
2025-08-14 17:54:58 +0200

    Add files via upload

--- scripts/myFileInfo.ps1
+++ scripts/myFileInfo.ps1
@@ -1,8 +1,9 @@
 <#
 	LAST CHANGED: 2025/08/14 - v1.00
 	
-	powershell -ep bypass -f myFileinfo.ps1 -Filepath c:\temp\tux.png
-	
+		
+	powershell "& {Import-Module '.\myFileinfo.ps1'; Get-FileInfo -Filepath c:\temp\tux.png}"
+		
 	powershell "iex (iwr https://raw.githubusercontent.com/hirosec/powertools/refs/heads/main/scripts/myFileInfo.ps1 -UseBasicParsing);  Get-FileInfo -Filepath c:\temp\tux.png"
 
 	powershell "iex (iwr https://tinyurl.com/7ae2e35r -UseBasicParsing);  Get-FileInfo -Filepath c:\temp\tux.png"
@@ -13,12 +14,28 @@ param (
 )
 
 
-$Signature =  @"
+Function Convert-Size {
+    [cmdletbinding()]
+    Param (
+        [parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [Alias("Length")]
+        [int64]$Size
+    )
+    Begin {
+        If (-Not $ConvertSize) {
+            $Signature =  @"
                  [DllImport("Shlwapi.dll", CharSet = CharSet.Auto)]
                  public static extern long StrFormatByteSize( long fileSize, System.Text.StringBuilder buffer, int bufferSize );
 "@
-$Global:ConvertSize = Add-Type -Name SizeConverter -MemberDefinition $Signature -PassThru
-
+            $Global:ConvertSize = Add-Type -Name SizeConverter -MemberDefinition $Signature -PassThru
+        }
+        $stringBuilder = New-Object Text.StringBuilder 1024
+    }
+    Process {
+        $ConvertSize::StrFormatByteSize( $Size, $stringBuilder, $stringBuilder.Capacity ) | Out-Null
+        $stringBuilder.ToString() + " ($($Size.ToString('N0')) bytes)"
+    }
+}
 
 
 function Get-FileInfo {
@@ -31,14 +48,12 @@ function Get-FileInfo {
 	
 		$fileInfo = Get-Item $filepath
 
-		$stringBuilder = New-Object Text.StringBuilder 1024
-		$ConvertSize::StrFormatByteSize( $($fileInfo.Length), $stringBuilder, $stringBuilder.Capacity ) | Out-Null
-		$SizeStr = $stringBuilder.ToString() + " ($($fileInfo.Length.ToString('N0')) bytes)"
-
 		Write-Host "File  : $file"
-		Write-Host "Size  : $SizeStr" 
+		Write-Host "Size  : $($($fileInfo.Length) | Convert-Size )" 
 		Write-Host "MD5   : $((Get-Filehash $($fileInfo.FullName) -algo md5).Hash)" 
 	} else {
 		Write-Host "ERROR - File `'$FilePath`' NOT found !"
 	}
 }
+
+
