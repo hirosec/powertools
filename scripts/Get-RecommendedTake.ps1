24a5833f2236c123dfd3355082a347bf3ba72463
hirosec <90599855+hirosec@users.noreply.github.com>
2025-09-17 12:35:11 +0200

    Add files via upload

---
+++ scripts/Get-RecommendedTake.ps1
@@ -0,0 +1,66 @@
+<#
+	Last Changed: 2025-09-17  - v1.0
+#>
+
+Function Get-RecommendedTake {
+
+	$urlList = @(
+		"https://sc1.checkpoint.com/documents/Jumbo_HFA/R81.10/R81.10/R81.10_Downloads.htm",
+		"https://sc1.checkpoint.com/documents/Jumbo_HFA/R81.20/R81.20/R81.20_Downloads.htm",
+		"https://sc1.checkpoint.com/documents/Jumbo_HFA/R82/R82.00/R82_Downloads.htm"
+	)
+	
+
+
+	$JumboInfo = @()	
+	
+	$urlList | % {
+			$url = $_
+	
+			try {
+				$response = Invoke-WebRequest -Uri $url #-ErrorAction SilentlyContinue -UseBasicParsing -DisableKeepAlive
+			} catch {
+				$response = $null
+			}
+ 
+			# If the HTTP status code is 200 (OK), write "OK" to the console with a green background
+			if ($response.StatusCode -eq 200) {
+
+				$response.RawContent -Split "`n" | % {
+				$line = $_
+
+				$posIndex = $line.IndexOf('<h1>')
+
+				If ($posIndex -ne -1) {
+					$cleanText = $line -replace '<[^>]+>', ''
+					$Version = $cleanText.Replace(' Jumbo Hotfix Accumulator Downloads','').Trim()
+				}
+
+				$posIndex = $line.IndexOf('<h3>')
+
+				If ($posIndex -ne -1) {
+					$cleanText = $line -replace '<[^>]+>', ''
+		
+					if ($cleanText -like "*Recommended*") {
+						$RecommdedTake = $cleanText.Replace(' - Recommended','').Trim()
+					}
+		
+					if ($cleanText -like "*Latest*") {
+						$LatestTake = $cleanText.Replace(' - Latest','').Trim()
+					}
+				}
+			}
+	
+	
+			$JumboInfo += [PSCustomObject]@{
+					Version      = "$($version)"
+					Recommended  = "$($RecommdedTake)"
+					Latest       = "$($LatestTake)"
+			}
+		}
+	}
+
+	$JumboInfo
+}
+
+
